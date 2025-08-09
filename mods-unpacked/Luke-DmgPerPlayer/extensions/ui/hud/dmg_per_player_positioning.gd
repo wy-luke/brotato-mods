a2ecd518823678d7861f7de45120a790f3d03eb0
wy-luke <wykj.luke@gmail.com>
2025-08-09 15:42:08 +0800

    clean code

--- mods-unpacked/Luke-DmgPerPlayer/extensions/ui/hud/dmg_per_player_positioning.gd
+++ mods-unpacked/Luke-DmgPerPlayer/extensions/ui/hud/dmg_per_player_positioning.gd
@@ -1,15 +1,9 @@
-# 继承自 "res://ui/hud/player_ui_elements.gd"
 extends "res://ui/hud/player_ui_elements.gd"
 
-# 设置HUD的位置
 func set_hud_position(position_index: int) -> void:
-	# 调用原始函数
 	.set_hud_position(position_index)
-	# 扩展
-	# 判断是否在底部
+	
 	var bottom = position_index > 1
-	# 获取伤害统计容器
 	var dmg_per_player_container = hud_container.get_node("DmgPerPlayerContainerP%s" % str(player_index + 1))
-	# 如果在底部，则将伤害统计容器移动到最前面
 	if bottom:
 		hud_container.move_child(dmg_per_player_container, 0)
--- mods-unpacked/Luke-DmgPerPlayer/ui/hud/total_dmg_display.gd
+++ mods-unpacked/Luke-DmgPerPlayer/ui/hud/total_dmg_display.gd
@@ -6,13 +6,13 @@ onready var total_value_label: Label = $Total/Value
 
 
 var player_index: int
-var wave_start_value: Dictionary = {}
+var item_wave_start_value: Dictionary = {}
 
 # 物品（除炮塔）或角色记录的是总伤害，所以需要回合开始时的数值计算本回合的伤害
 func track_item(item: ItemParentData, index: int) -> void:
 	player_index = index
 	if [Category.ITEM, Category.CHARACTER].has(item.get_category()) && not item.name == "ITEM_BUILDER_TURRET":
-		wave_start_value[item.my_id] = RunData.tracked_item_effects[player_index][item.my_id]
+		item_wave_start_value[item.my_id] = RunData.tracked_item_effects[player_index][item.my_id]
 
 
 func get_item_dmg_dealt(item: ItemParentData) -> int:
@@ -20,13 +20,13 @@ func get_item_dmg_dealt(item: ItemParentData) -> int:
 		return item.dmg_dealt_last_wave
 	else:
 		var start_value = 0
-		if wave_start_value.has(item.my_id):
-			start_value = wave_start_value[item.my_id]
+		if item_wave_start_value.has(item.my_id):
+			start_value = item_wave_start_value[item.my_id]
 		return RunData.tracked_item_effects[player_index][item.my_id] - start_value
 
 func set_total_damage(damage: int) -> void:
 	wave_value_label.text = Text.get_formatted_number(damage)
-	total_value_label.text = Text.get_formatted_number(damage)
+	# total_value_label.text = Text.get_formatted_number(damage)
 
 func set_hud_position(position_index: int) -> void:
 	var left = position_index == 0 or position_index == 2
