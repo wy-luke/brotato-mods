cb4b5812c431b68fa8b2916631f3b5af291025af
wy-luke <wykj.luke@gmail.com>
2025-08-17 21:15:32 +0800

    refactor code

--- mods-unpacked/Luke-DmgPerPlayer/extensions/entities/units/enemies/enemy.gd
+++ mods-unpacked/Luke-DmgPerPlayer/extensions/entities/units/enemies/enemy.gd
@@ -1,12 +1,10 @@
 extends "res://entities/units/enemies/enemy.gd"
 
 func take_damage(value: int, args: TakeDamageArgs) -> Array:
-	var damage_taken :=.take_damage(value, args)
+	var damage_taken =.take_damage(value, args)
 
-	var full_dmg_value: int = damage_taken[0]
-	ChallengeService.try_complete_challenge("chal_overkill", full_dmg_value)
-
-	RunData.player_damage[args.from_player_index] += damage_taken[1]
-	RunData.player_damage_total[args.from_player_index] += damage_taken[1]
+	if args.from_player_index != -1:
+		RunData.player_damage[args.from_player_index] += damage_taken[1]
+		RunData.player_damage_total[args.from_player_index] += damage_taken[1]
 	
 	return damage_taken
--- mods-unpacked/Luke-DmgPerPlayer/ui/hud/dmg_per_player_container.gd
+++ mods-unpacked/Luke-DmgPerPlayer/ui/hud/dmg_per_player_container.gd
@@ -25,29 +25,31 @@ func init(p_index: int) -> void:
 	total_container.alignment = BoxContainer.ALIGN_BEGIN if left else BoxContainer.ALIGN_END
 
 func _update_display() -> void:
-	if player_index == -1 or player_index >= RunData.get_player_count():
+	var player_count = RunData.get_player_count()
+	if player_index == -1 or player_index >= player_count:
 		return
 
-	if RunData.get_player_count() > 1:
-		var total_wave_damage = 0
-		for i in RunData.get_player_count():
-			total_wave_damage += RunData.player_damage[i]
+	var wave_damage = RunData.player_damage[player_index]
+	var total_damage = RunData.player_damage_total[player_index]
 
-		var wave_percentage = 0
-		if total_wave_damage > 0:
-			wave_percentage = int(float(RunData.player_damage[player_index]) / total_wave_damage * 100)
-		
+	if player_count <= 1:
+		wave_damage_label.text = str(wave_damage)
+		total_damage_label.text = str(total_damage)
+		return
+
+	var wave_damage_all_players = 0
+	var total_damage_all_players = 0
+	for i in range(player_count):
+		wave_damage_all_players += RunData.player_damage[i]
+		total_damage_all_players += RunData.player_damage_total[i]
 	
-		var total_damage_all_players = 0
-		for i in RunData.get_player_count():
-			total_damage_all_players += RunData.player_damage_total[i]
-		
-		var total_percentage = 0
-		if total_damage_all_players > 0:
-			total_percentage = int(float(RunData.player_damage_total[player_index]) / total_damage_all_players * 100)
+	_set_label_text(wave_damage_label, wave_damage, wave_damage_all_players)
+	_set_label_text(total_damage_label, total_damage, total_damage_all_players)
 
-		wave_damage_label.text = "%s (%s%%)" % [RunData.player_damage[player_index], wave_percentage]
-		total_damage_label.text = "%s (%s%%)" % [RunData.player_damage_total[player_index], total_percentage]
-	else:
-		wave_damage_label.text = str(RunData.player_damage[player_index])
-		total_damage_label.text = str(RunData.player_damage_total[player_index])
+
+func _set_label_text(label: Label, damage: int, total_damage: int) -> void:
+	var percentage = 0
+	if total_damage > 0:
+		percentage = int(float(damage) / total_damage * 100)
+	
+	label.text = "%s (%s%%)" % [damage, percentage]
