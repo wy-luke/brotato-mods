eb3881601f010e82825f1ac4d18c47273e349e67
wy-luke <wykj.luke@gmail.com>
2026-01-11 00:14:47 +0800

    feat: add translation support

--- mods-unpacked/Luke-DmgPerPlayer/mod_main.gd
+++ mods-unpacked/Luke-DmgPerPlayer/mod_main.gd
@@ -5,6 +5,7 @@ const LUKE_DMGPERPLAYER_LOG_NAME := "Luke-DmgPerPlayer:Main"
 
 var mod_dir_path := ""
 var extensions_dir_path := ""
+var translations_dir_path := ""
 
 func _init() -> void:
 	mod_dir_path = ModLoaderMod.get_unpacked_dir().plus_file(LUKE_DMGPERPLAYER_DIR)
@@ -13,6 +14,12 @@ func _init() -> void:
 	ModLoaderMod.install_script_extension(extensions_dir_path.plus_file("main.gd"))
 	ModLoaderMod.install_script_extension(extensions_dir_path.plus_file("singletons/run_data.gd"))
 	ModLoaderMod.install_script_extension(extensions_dir_path.plus_file("entities/units/enemies/enemy.gd"))
+	add_translations()
 
 func _ready() -> void:
 	ModLoaderLog.info("Ready!", LUKE_DMGPERPLAYER_LOG_NAME)
+
+func add_translations() -> void:
+  translations_dir_path = mod_dir_path.plus_file("translations")
+  ModLoaderMod.add_translation(translations_dir_path.plus_file("translations.en.translation"))
+  ModLoaderMod.add_translation(translations_dir_path.plus_file("translations.zh_Hans_CN.translation"))
