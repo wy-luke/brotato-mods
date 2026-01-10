45f2574a9b2d6ab333daeb9149265e384c2ce4e4
wy-luke <wykj.luke@gmail.com>
2026-01-11 00:40:48 +0800

    fix: update player_damage firstly in run_data

--- mods-unpacked/Luke-DmgPerPlayer/extensions/singletons/run_data.gd
+++ mods-unpacked/Luke-DmgPerPlayer/extensions/singletons/run_data.gd
@@ -4,10 +4,10 @@ var player_damage: Array = [0, 0, 0, 0]
 var player_damage_total: Array = [0, 0, 0, 0]
 
 func reset(restart: bool = false):
-	.reset(restart)
 	player_damage = [0, 0, 0, 0]
 	player_damage_total = [0, 0, 0, 0]
+	.reset(restart)
 
 func on_wave_start(timer: WaveTimer) -> void:
-	.on_wave_start(timer)
 	player_damage = [0, 0, 0, 0]
+	.on_wave_start(timer)
