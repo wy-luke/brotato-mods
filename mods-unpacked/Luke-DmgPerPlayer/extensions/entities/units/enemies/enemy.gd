da51641f321b942e5681c239653e037ed37771f4
wy-luke <wykj.luke@gmail.com>
2026-01-10 23:11:10 +0800

    fix(enemy): handle player index bounds when tracking damage

--- mods-unpacked/Luke-DmgPerPlayer/extensions/entities/units/enemies/enemy.gd
+++ mods-unpacked/Luke-DmgPerPlayer/extensions/entities/units/enemies/enemy.gd
@@ -2,9 +2,12 @@ extends "res://entities/units/enemies/enemy.gd"
 
 func take_damage(value: int, args: TakeDamageArgs) -> Array:
 	var damage_taken =.take_damage(value, args)
+	var damages = damage_taken[1]
 
-	if args.from_player_index != -1:
-		RunData.player_damage[args.from_player_index] += damage_taken[1]
-		RunData.player_damage_total[args.from_player_index] += damage_taken[1]
+	var p_index = args.from_player_index
+
+	if p_index >= 0 and p_index < RunData.player_damage.size():
+		RunData.player_damage[p_index] += damages
+		RunData.player_damage_total[p_index] += damages
 	
 	return damage_taken
