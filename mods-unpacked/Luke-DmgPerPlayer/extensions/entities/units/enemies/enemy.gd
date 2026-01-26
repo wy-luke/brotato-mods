b24c7e9f536001629ab366e2279412907ac33dd0
wy-luke <wykj.luke@gmail.com>
2026-01-26 09:48:43 +0800

    fix: add charmed enemies' damage

--- mods-unpacked/Luke-DmgPerPlayer/extensions/entities/units/enemies/enemy.gd
+++ mods-unpacked/Luke-DmgPerPlayer/extensions/entities/units/enemies/enemy.gd
@@ -6,6 +6,13 @@ func take_damage(value: int, args: TakeDamageArgs) -> Array:
 
 	var p_index = args.from_player_index
 
+	if p_index < 0 or p_index >= RunData.player_damage.size():
+		var hitbox = args.hitbox
+		if hitbox and is_instance_valid(hitbox.from) and hitbox.from is Enemy:
+			var charmed_by = hitbox.from.get_charmed_by_player_index()
+			if charmed_by >= 0 and charmed_by < RunData.player_damage.size():
+				p_index = charmed_by
+
 	if p_index >= 0 and p_index < RunData.player_damage.size():
 		RunData.player_damage[p_index] += damages
 		RunData.player_damage_total[p_index] += damages
